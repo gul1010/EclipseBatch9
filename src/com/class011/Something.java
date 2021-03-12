54429e599c30430c0162280df596d1eda5e36b74
gul1010 <gulichka_rd@yahoo.com>
2021-03-11 20:14:58 -0800

    Another commit

---
+++ src/com/class011/Something.java
@@ -0,0 +1,16 @@
+package com.class011;
+
+public class Something {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte	box1=122; //-127 till 129
+		   
+		   short box2=32767; //-32768 till 32767
+		   
+		   int box3=1000;
+		   box3=2000;
+		   long box4=1000000000000l;
+	}
+
+}
