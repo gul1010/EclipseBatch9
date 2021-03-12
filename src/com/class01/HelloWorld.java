18d80db383c9b9412dc3cbe8d73d117a04f44db6
gul1010 <gulichka_rd@yahoo.com>
2021-03-11 20:03:31 -0800

    The first commit

---
+++ src/com/class01/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.class01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+        //this a single comment
+		System.out.println("Hello Gul !!!");
+
+		System.out.println("Nice to see you always");
+
+		System.out.println("what will you be going to do tonight? Any Dinner");
+
+		/*multiline comments
+		 * jajajaaj
+		 * lalala
+		 */
+		
+	}	
+	
+}
